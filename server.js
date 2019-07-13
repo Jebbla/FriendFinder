66ddcce5cefa6cfc3f22a21a052e0384f4a26c94
Jebbla <jlp2722@gmail.com>
2019-07-12 19:18:14 -0500

    fixed some bugs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
-
+var path = require("path");
+var bodyParser = require('body-parser');
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -11,11 +12,32 @@ var app = express();
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static('./app/public'));
+
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Body Parser 
+// =============================================================
+app.use(bodyParser.urlencoded({ extended: false })); 
+// parse application/json
+app.use(bodyParser.json());
+
+// Include routes
+// =============================================================
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
 
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
+
+
+
+
+
